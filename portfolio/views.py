4d4ba39f608a0e0e03868f04c865e99ae720ae53
Anthony Ueta Vecchia <tony.ueta@gmail.com>
2025-06-22 01:16:10 -0300

    Creating About Me and Featured Projects section along with some reponsiveness improvements at _base.html and fixing datetime call at view.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return render(request, 'portfolio/index.html')
+    return render(request, 'portfolio/index.html', {'now':datetime.now()})
